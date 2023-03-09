5544be5e552e8952ee4e2891d1fa06983e244395
Okbede123 <116688986+Okbede123@users.noreply.github.com>
2023-03-09 22:39:06 +0700

    publish nhanh o nha 9/3

--- app/src/androidTest/java/InterFaceUI/CreateInvoiceOrdersPageUI.java
+++ app/src/androidTest/java/InterFaceUI/CreateInvoiceOrdersPageUI.java
@@ -5,6 +5,13 @@ public class CreateInvoiceOrdersPageUI {
     public static final String CLICK_TO_CREATE_ORDER = "xpath="+ BaseUI.TAG_DYNAMIC_IMAGE_BUTTON+ "[@resource-id='net.citigo.kol.free:id/fabAdd']";
     public static final String CLICK_ADD_PICTURES = "xpath="+ BaseUI.TAG_DYNAMIC_IMAGE_VIEW+"[@resource-id='net.citigo.kol.free:id/ivPhoto']";
     public static final String INPUT_NAME_ITEM = "xpath="+BaseUI.TAG_DYNAMIC_WIDGET_EDITTEXT+ "[@resource-id='net.citigo.kol.free:id/etName']";
+    public static final String INPUT_CATEGORY = "xpath="+BaseUI.TAG_DYNAMIC_WIDGET_EDITTEXT+ "[@resource-id='net.citigo.kol.free:id/etCategory']";
 
+    public static final String ADD_CATEGORY = "xpath="+BaseUI.TAG_DYNAMIC_IMAGE_BUTTON+"[@resource-id='net.citigo.kol.free:id/fabAdd']";
+
+    public static final String PRICE_SELL = "xpath="+BaseUI.TAG_DYNAMIC_WIDGET_EDITTEXT+"[@resource-id='net.citigo.kol.free:id/etPrice']";
+
+    public static final String PRICE_SELL_COST = "xpath="+BaseUI.TAG_DYNAMIC_WIDGET_EDITTEXT+"[@resource-id='net.citigo.kol.free:id/etPrice']";
+    public static final String ON_HAND = "xpath="+BaseUI.TAG_DYNAMIC_WIDGET_EDITTEXT+"[@resource-id='net.citigo.kol.free:id/etOnHand']";
 
 }
--- app/src/androidTest/java/actions/CreateInvoiceOrdersPageObject.java
+++ app/src/androidTest/java/actions/CreateInvoiceOrdersPageObject.java
@@ -23,8 +23,22 @@ public class CreateInvoiceOrdersPageObject extends BasePage {
         return PageGeneralManager.openSelectPicturePage(driver);
     }
 
-    public void inputInformationItems(String nameItems){
+    public void inputInformationItems(String nameItems,String nameGroup,String chooseGroupParent,String priceSells,String priceCost,String onHand){
         sendKeysToElement(CreateInvoiceOrdersPageUI.INPUT_NAME_ITEM,nameItems);
+        clickToElements(CreateInvoiceOrdersPageUI.INPUT_CATEGORY);
+        clickToElements(CreateInvoiceOrdersPageUI.ADD_CATEGORY);
+        sendKeysToElement(CreateInvoiceOrdersPageUI.INPUT_NAME_ITEM,nameGroup);
+        clickToElements(CreateInvoiceOrdersPageUI.INPUT_CATEGORY);
+        clickToElements(BaseUI.DYNAMIC_WIDGET_TEXTVIEW,chooseGroupParent);
+        clickToElements(BaseUI.DYNAMIC_WIDGET_BUTTON,"Lưu");
+        clickToElements(BaseUI.DYNAMIC_WIDGET_TEXTVIEW,chooseGroupParent);
+        sendKeysToElement(CreateInvoiceOrdersPageUI.PRICE_SELL,priceSells);
+        sendKeysToElement(CreateInvoiceOrdersPageUI.PRICE_SELL_COST,priceCost);
+        sendKeysToElement(CreateInvoiceOrdersPageUI.ON_HAND,onHand);
+        clickToElements(BaseUI.DYNAMIC_WIDGET_BUTTON,"Lưu");
+
+
+
     }
 
 }
